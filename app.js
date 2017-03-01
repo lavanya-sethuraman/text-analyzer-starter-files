a672caf484892f3b43dd4a2b6ca749561d034281
lavanya-sethuraman <lavanya.sethuraman23@gmail.com>
2017-02-28 22:04:04 -0500

    Text Analyzer Challenge

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
-// your code here!
+$(function(){
+
+  getStats();
+});
+
+
+
+
+function getStats()
+{
+	$('button').click(function(event){
+	event.preventDefault();
+
+	var userText=$('#user-text').val();
+	var words=getWords(userText);
+
+	var totalWords=words.length;
+	var uniqueWordCount=getUniqueWords(words);
+	var averageWordLength=getAverage(words);
+	$('.text-report').find('.js-word-count').text(totalWords);
+	$('.text-report').find('.js-unique-words').text(uniqueWordCount);
+	$('.text-report').find('.js-average-length').text(averageWordLength);
+	$('.text-report').removeClass('hidden');
+});
+}
+
+function getWords(userText)
+{
+  return userText.split(/[ ,.!":;-]+/).filter(Boolean);
+}
+
+function getUniqueWords(words)
+{
+  var uniqueWords={};
+  for(var i=0;i<words.length;i++)
+  {
+    if(words in uniqueWords)
+      uniqueWords[words[i]]++;
+    else
+      uniqueWords[words[i]]=1;
+  }
+
+  return Object.keys(uniqueWords).length;
+
+}
+
+function getAverage(words)
+{
+  var total=0;
+  for(var i=0;i<words.length;i++)
+    {
+      total+=words[i].length;
+    }
+  return total/words.length;
+}
